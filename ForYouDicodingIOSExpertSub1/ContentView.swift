7ca1011f945001d278c3388ec20f6894b6e0277e
helloail <zinnuratthufail2@gmail.com>
2020-11-01 12:25:18 +0700

    Initial Commit

---
+++ ForYouDicodingIOSExpertSub1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForYouDicodingIOSExpertSub1
+//
+//  Created by Moh Zinnur Atthufail Addausi on 01/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
